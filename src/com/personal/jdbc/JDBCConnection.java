09feb8c01c06e469df0965ba236b5106e56c600e
smamdapure@gmail.com <Reliance.123#>
2019-10-20 23:31:24 +0530

    Added method for database connection

--- src/com/personal/jdbc/JDBCConnection.java
+++ src/com/personal/jdbc/JDBCConnection.java
@@ -1,17 +1,16 @@
 package com.personal.jdbc;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.*;
 
-
+/**
+ * This class handles database operations
+ */
 public class JDBCConnection {
+  private static String url = "jdbc:mysql://127.0.0.1:3306/employees_database";
+
   public static void main(String[] args) {
-    String url = "jdbc:mysql://127.0.0.1:3306/employees_database";
     try {
-      Connection connection = DriverManager.getConnection(url, "root", "root123");
+      Connection connection = getConnection("root", "root123");
       Statement statement = connection.createStatement();
 
       ResultSet resultSet = statement.executeQuery("select * from employees_tbl");
@@ -21,5 +20,21 @@ public class JDBCConnection {
     } catch (SQLException e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * @param userName username of the database
+   * @param password password of the database
+   * @return database connection object
+   */
+  private static Connection getConnection(String userName, String password) {
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection(url, userName, password);
+      return connection;
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return connection;
   }
 }
